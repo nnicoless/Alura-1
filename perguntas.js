f552c70747399c9c5d1ae90399425d70c7270d3f
nnicoless <nicole.bugdanovicz.luz@escola.pr.gov.br>
2025-05-20 07:37:01 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão',
+    'A capital do Japão é Tokyio'
+)
+
+criaCartao(
+    Programação,
+    'O que é CSS?',
+    'CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
